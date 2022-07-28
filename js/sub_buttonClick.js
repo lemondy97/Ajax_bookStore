876e192e832f0d74f39dad0f035c1cd181309ad6
lemondy97 <104114377+lemondy97@users.noreply.github.com>
2022-07-28 12:51:06 +0900

    20220728_project4_jingyu5
    서브 업데이트

--- js/sub_buttonClick.js
+++ js/sub_buttonClick.js
@@ -10,5 +10,10 @@
                 event.preventDefault();
                 });
 
+	   $('#image_button').click(function(){
+                    $('#image_button').hide();
+                    $('#bookImage_space').css("height", "2950px");
+                });
+
 
             });
